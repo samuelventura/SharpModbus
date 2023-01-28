d7583edff7d9471ca352d25d58b713f9de2052c0
Samuel Ventura <samuel.ventura@yeico.com>
2023-01-28 13:31:22 -0600

    Added socat based serial testing for Linux/MacOS. Only tested on Linux so far.

---
+++ SharpModbus.Test.Socat/Socat.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using NUnit.Framework;
+using SharpModbus.Serial;
+using SharpModbus;
+
+namespace SharpModbus.Test.Special
+{
+    public class Socat: IDisposable
+    {
+        public const string Master = "/tmp/socat.master";
+        public const string Slave = "/tmp/socat.slave";
+
+        private readonly Process process;
+
+        public Socat()
+        {
+            process = new Process();
+            process.StartInfo = new ProcessStartInfo()
+            {
+                FileName = "socat",
+                Arguments = $"-d -d pty,link={Master} pty,link={Slave}",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            };
+            process.Start();
+            Dump(process.StandardOutput);
+            Dump(process.StandardError);
+            Wait(Master);
+            Wait(Slave);
+        }
+
+        public void Dispose()
+        {
+            Tools.Try(() =>
+            {
+                process.StandardInput.Close();
+                process.WaitForExit(200);
+            });
+            Tools.Try(process.Kill);
+            Tools.Try(process.Dispose);
+            File.Delete(Master);
+            File.Delete(Slave);
+        }
+
+        private void Dump(StreamReader reader)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                var line = reader.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = reader.ReadLine();
+                }
+            });
+        }
+
+        private void Wait(string path) 
+        {
+            var dl  = DateTime.Now.AddMilliseconds(2000);
+            while(!File.Exists(path)) 
+            {
+                if (DateTime.Now > dl) throw Tools.Make("Missing {0}", path);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ SharpModbus.Test.Socat/SocatSlaveTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using NUnit.Framework;
+using SharpModbus.Serial;
+
+namespace SharpModbus.Test.Special
+{
+    public class SocatSlaveTest
+    {
+        private SerialSettings ss(string portName)
+        {
+            return new SerialSettings { PortName = portName };
+        }
+
+        [Test]
+        public void TcpOverSerialTest()
+        {
+            var model = new ModbusModel();
+            var scanner = new ModbusTCPScanner();
+            using(var socat = new Socat())
+            using (var server = new SerialModel(Socat.Slave, model, scanner))
+            using (var stream = new ModbusSerialStream(ss(Socat.Master), 400))
+            {
+                var master = new ModbusMaster(stream, new ModbusTCPProtocol());
+                //race condition avoided by access order
+                H.SharedSlaveTest(model, master);
+            }
+        }
+
+        [Test]
+        public void RtuOverSerialTest()
+        {
+            var model = new ModbusModel();
+            var scanner = new ModbusRTUScanner();
+            using(var socat = new Socat())
+            using (var server = new SerialModel(Socat.Slave, model, scanner))
+            using (var master = ModbusMaster.RTU(ss(Socat.Master)))
+            {
+                //race condition avoided by access order
+                H.SharedSlaveTest(model, master);
+            }
+        }
+
+        [Test]
+        public void TcpExceptionOverSerialTest()
+        {
+            var model = new ModbusModel();
+            var scanner = new ModbusTCPScanner();
+            using(var socat = new Socat())
+            using (var server = new SerialModel(Socat.Slave, model, scanner))
+            using (var stream = new ModbusSerialStream(ss(Socat.Master), 400))
+            {
+                var master = new ModbusMaster(stream, new ModbusTCPProtocol());
+                var ex = Assert.Throws<ModbusException>(() => H.SharedExceptionTest(master));
+                Assert.AreEqual("Modbus exception 2", ex.Message);
+                Assert.AreEqual(2, ex.Code);
+            }
+        }
+
+        [Test]
+        public void RtuExceptionOverSerialTest()
+        {
+            var model = new ModbusModel();
+            var scanner = new ModbusRTUScanner();
+            using(var socat = new Socat())
+            using (var server = new SerialModel(Socat.Slave, model, scanner))
+            using (var master = ModbusMaster.RTU(ss(Socat.Master)))
+            {
+                var ex = Assert.Throws<ModbusException>(() => H.SharedExceptionTest(master));
+                Assert.AreEqual("Modbus exception 2", ex.Message);
+                Assert.AreEqual(2, ex.Code);
+            }
+        }
+    }
+}
